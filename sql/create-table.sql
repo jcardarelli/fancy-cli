dacc6cd5594c2420ebd49d805672d79af90b8ead
John Cardarelli <jcardarelli@users.noreply.github.com>
2023-09-21 19:03:16 -0700

    Reformat with sleek

--- sql/create-table.sql
+++ sql/create-table.sql
@@ -1,12 +1,19 @@
-create table [restaurants] (
-	id INTEGER NOT NULL PRIMARY KEY,
-	name TEXT,
-	address TEXT,
-	stars INTEGER
+CREATE TABLE [restaurants] (
+    id INTEGER NOT NULL PRIMARY KEY,
+    name TEXT,
+    address TEXT,
+    stars INTEGER
 );
 
+-- sample data
+INSERT INTO
+    restaurants(name, address, stars)
+VALUES
+(
+        'The French Laundry',
+        '6640 Washington St, Yountville, CA 94599',
+        3
+    );
 
-sqlite> insert into restaurants(name, address, stars) values('The French Laundry', '6640 Washington St, Yountville, CA 94599', 3);
-sqlite> select * from restaurants;
-1|The French Laundry|6640 Washington St, Yountville, CA 94599|3
-
+-- SELECT * FROM restaurants;
+-- 1 | The French Laundry | 6640 Washington St, Yountville, CA 94599 | 3
